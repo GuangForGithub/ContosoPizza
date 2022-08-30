b4f5079bcd7a7095cd2090ccbfa86d90b7f12e8e
GuangForGithub <guangms@hotmail.com>
2022-08-30 23:43:57 +0800

    完成mysql数据库创建

---
+++ Migrations/20220830153753_ModelRevisions.cs
@@ -0,0 +1,201 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoPizza.Migrations
+{
+    public partial class ModelRevisions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsGlutenFree",
+                table: "Pizzas");
+
+            migrationBuilder.UpdateData(
+                table: "Toppings",
+                keyColumn: "Name",
+                keyValue: null,
+                column: "Name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Toppings",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Calories",
+                table: "Toppings",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Sauces",
+                keyColumn: "Name",
+                keyValue: null,
+                column: "Name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sauces",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVegan",
+                table: "Sauces",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Pizzas",
+                keyColumn: "Name",
+                keyValue: null,
+                column: "Name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SauceId",
+                table: "Pizzas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "PizzaTopping",
+                columns: table => new
+                {
+                    PizzasId = table.Column<int>(type: "int", nullable: false),
+                    ToppingsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PizzaTopping", x => new { x.PizzasId, x.ToppingsId });
+                    table.ForeignKey(
+                        name: "FK_PizzaTopping_Pizzas_PizzasId",
+                        column: x => x.PizzasId,
+                        principalTable: "Pizzas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PizzaTopping_Toppings_ToppingsId",
+                        column: x => x.ToppingsId,
+                        principalTable: "Toppings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pizzas_SauceId",
+                table: "Pizzas",
+                column: "SauceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PizzaTopping_ToppingsId",
+                table: "PizzaTopping",
+                column: "ToppingsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pizzas_Sauces_SauceId",
+                table: "Pizzas",
+                column: "SauceId",
+                principalTable: "Sauces",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pizzas_Sauces_SauceId",
+                table: "Pizzas");
+
+            migrationBuilder.DropTable(
+                name: "PizzaTopping");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pizzas_SauceId",
+                table: "Pizzas");
+
+            migrationBuilder.DropColumn(
+                name: "Calories",
+                table: "Toppings");
+
+            migrationBuilder.DropColumn(
+                name: "IsVegan",
+                table: "Sauces");
+
+            migrationBuilder.DropColumn(
+                name: "SauceId",
+                table: "Pizzas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Toppings",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Sauces",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pizzas",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsGlutenFree",
+                table: "Pizzas",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ContosoPizza.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//将自定义 DbContext（此处是CarDbContext） 添加到 DI 容器
+builder.Services.AddDbContext<PizzaContext>(opt => opt.UseMySql(
+  builder.Configuration.GetConnectionString("MySqlConnection"),
+  new MySqlServerVersion(new Version("8.0.30"))));
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -12,8 +19,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
